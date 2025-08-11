56118f7d2b3179b754daf81ebc3f55cc7abbb9d9
Takeover713 <44397569+Takeover713@users.noreply.github.com>
2025-08-11 18:04:19 -0500

    dnc

--- Rafleshia_Dnc_Gear.lua
+++ Rafleshia_Dnc_Gear.lua
@@ -7,7 +7,7 @@ function user_setup()
     state.PhysicalDefenseMode:options('PDT')
 	state.MagicalDefenseMode:options('MDT')
 	state.ResistDefenseMode:options('MEVA')
-	state.Weapons:options('Gletis','Prime','Trials')
+	state.Weapons:options('Gletis','Prime','Twashtar')
 
     -- Additional local binds
     send_command('bind @` gs c step')
@@ -47,7 +47,7 @@ function init_gear_sets()
 	-- Weapons sets --
 	sets.weapons.Gletis = {main="Gleti's Knife",sub={ name="Fusetto +2", augments={'TP Bonus +1000',}},}
 	sets.weapons.Prime = {main="Mpu Gandring",sub="Fusetto +2"}
-	sets.weapons.Trials = {main="Twashtar",sub="Empty"}
+	sets.weapons.Twashtar = {main="Twashtar",sub="Fusetto +2"}
 	
 	------------------------------
 	  ------ Idle sets ------
